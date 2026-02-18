68ad382ae51952667ebc886d66460a77bb6d247c
Hidemasa317 <hidecatshark.0317@gmail.com>
2026-02-18 04:51:16 +0000

    Commit 2/18

---
+++ Project-8/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Project-8 React + TypeScript</h1>
+      <p>This is the entry point for your application.</p>
+    </div>
+  );
+}
