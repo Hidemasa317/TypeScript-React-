3b7b1912014409c9b7aded1998942544a213c0e4
Hidemasa317 <hidecatshark.0317@gmail.com>
2026-02-18 06:01:39 +0000

    Commit 2/18

--- Memo/p8.tsx
+++ Memo/p8.tsx
@@ -2,19 +2,19 @@
 
 import { useState } from 'react';
 
-export default function App() {
-  //✅📦id取得
+export default function Page() {
+  //📦id取得
   const [zip, setZip] = useState<string>('');
   const [addr, setAddr] = useState<string>('-');
   const [msg, setMsg] = useState<string>('');
 
-  // ✅🤖入力値受け取り部、論理演算子でnullなどの際に空文字に。
+  // 🤖入力値受け取り部、論理演算子でnullなどの際に空文字に。
   // /\D/g, ''でより整形している。
   function normalizeZip(zipValue: string) {
     return (zipValue || '').replace(/\D/g, '');
   }
 
-  //✅🤖api通信部
+  //🤖api通信部
   async function apiConnect(zipcode: string): Promise<string> {
     const url = `https://zipcloud.ibsnet.co.jp/api/search?zipcode=${encodeURIComponent(zipcode)}`;
     const res = await fetch(url);
@@ -24,13 +24,11 @@ export default function App() {
     return `${result.address1}${result.address2}${result.address3}`; //✅結合して返す。
   }
 
-  //✅🤖イベント登録部、エラー表示、　　addEventListener => handleClick
-  // クリック時に何が起きるかを設定。
+  //🤖イベント登録部、エラー表示、
   const handleClick = async () => {
-    setMsg(''); //  📦msg.textContent = '';
-    setAddr('-'); // 📦addr.textContent = '-';
+    setMsg('');
+    setAddr('-');
 
-    // 🚨エラー部
     const normalizedZip = normalizeZip(zip);
     if (normalizedZip.length !== 7) {
       setMsg('7桁で入力（ハイフン可）');
@@ -42,40 +40,27 @@ export default function App() {
   };
 
   return (
-    //   <label
-    //     ><input id="zip" inputmode="numeric" placeholder="郵便番号を入力してください。" />
-    //     <button id="btn">住所変換ボタン</button> </label
-    //   ><!-- 郵便番号入力欄 -->
-    //   <br />
-
-    //   <br />
-    //   <div class="box">
-    //     <div>住所: <span id="addr">-</span></div>
-    //     <div id="msg"></div>
-    //   </div>
-    // </div>
     <div
       id="container"
       style={{
         width: 400,
         margin: '0 auto',
-        border: '1px solid rgb(240,1,1)',
-        padding: '11px',
+        border: '1px solid rgb(240, 11, 11)',
+        padding: 16,
       }}
     >
-      <h1> Address </h1>
+      <h1>Address</h1>
       <label>
         <input
           value={zip}
-          onChange={(i) => setZip(i.target.value)}
+          onChange={(e) => setZip(e.target.value)}
           inputMode="numeric"
-          placeholder="郵便番号を入力してください"
+          placeholder="郵便番号を入力してください。"
         />
         <button onClick={handleClick}>住所変換ボタン</button>
       </label>
       <br />
       <br />
-
       <div
         className="box"
         style={{
@@ -86,12 +71,9 @@ export default function App() {
         }}
       >
         <div>
-          <span>: 住所 {addr}</span>
+          住所: <span>{addr}</span>
         </div>
-        <div>
-          {msg}
-          <div />
-        </div>
+        <div>{msg}</div>
       </div>
     </div>
   );
